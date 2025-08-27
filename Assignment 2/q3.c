8ebebdadf52f1bfe781c7b1d1cf9ac3738c9f003
Ayush_004 <159397801+AyushLakhapati@users.noreply.github.com>
2025-08-28 01:06:42 +0530

    Add files via upload

---
+++ Assignment 2/q3.c
@@ -0,0 +1,15 @@
+//Write a program to find greatest of three numbers using nested if-else.
+#include<stdio.h>
+void main(){
+int a=10,b=30,c=40;
+if(a>b&&a>c){
+	printf("a is the greater no");
+}
+  else if(b>a&&b>c){
+  	printf("b is the greater no");
+	  }
+  	else{
+  		printf("c is the greater no");
+	  }
+  
+}
